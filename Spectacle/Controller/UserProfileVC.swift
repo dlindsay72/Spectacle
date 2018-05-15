c484094e12082e2d2863c5546e01cadc3348209a
Dan Lindsay <dlindsay72@hotmail.com>
2018-05-15 17:35:16 -0400

    setup custom collectionViewCell of UserProfileHeader, added User struct for UserProfileVC, added imageView for profile image into our UserProfileHeader. Data successfully loads from Firebase

--- Spectacle/Controller/UserProfileVC.swift
+++ Spectacle/Controller/UserProfileVC.swift
@@ -9,33 +9,71 @@
 import UIKit
 import Firebase
 
+public let headerIdentifier = "headerID"
 class UserProfileVC: UICollectionViewController {
+    
+    var user: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         collectionView?.delegate = self
-        
-      //  navigationItem.title = Auth.auth().currentUser?.uid
-        
+        navigationItem.title = Auth.auth().currentUser?.uid
         fetchUser()
         
+        collectionView?.register(UserProfileHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerIdentifier)
+        
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! UserProfileHeader
+        header.user = self.user
+        return header
+    }
+    
+    
         
     fileprivate func fetchUser() {
-        
         guard let uid = Auth.auth().currentUser?.uid else { return }
         Database.database().reference().child("users").child(uid).observeSingleEvent(of: .value, with: { (snapshot) in
             print(snapshot.value ?? "")
             guard let dictionary = snapshot.value as? [String: Any] else { return }
-            let username = dictionary["username"] as? String
             
-            self.navigationItem.title = username?.capitalized
+            self.user = User(dictionary: dictionary)
+            self.navigationItem.title = self.user?.username.capitalized
+            self.collectionView?.reloadData()
         }) { (error) in
             print("Failed to fetch user:", error)
         }
     }
-    
-    
 }
+
+extension UserProfileVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
+}
+
+struct User {
+    let username: String
+    let profileImageUrl: String
+    
+    init(dictionary: [String: Any]) {
+        self.username = dictionary["username"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Spectacle/View/UserProfileHeader.swift
@@ -0,0 +1,72 @@
+//
+//  UserProfileHeader.swift
+//  Spectacle
+//
+//  Created by Dan Lindsay on 2018-05-15.
+//  Copyright © 2018 Dan Lindsay. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class UserProfileHeader: UICollectionViewCell {
+    
+    var user: User? {
+        didSet {
+            setupProfileImage()
+        }
+    }
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .blue
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 12, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 80, height: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        profileImageView.clipsToBounds = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupProfileImage() {
+        guard let profileImageUrl = user?.profileImageUrl else { return }
+        guard let url = URL(string: profileImageUrl) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            //check for error, then construct the image using data
+            if let error = error {
+                print("Failed to fetch image profile image:", error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            let image = UIImage(data: data)
+            print(data)
+            DispatchQueue.main.async {
+                self.profileImageView.image = image
+            }
+        }.resume()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
