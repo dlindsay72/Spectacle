574c022407850ee770ec5e22099a8411b4af2156
Dan Lindsay <dlindsay72@hotmail.com>
2018-07-31 14:56:44 -0400

    implemented an inputAccessoryView for the comments text field

--- Spectacle/Controller/CommentsVC.swift
+++ Spectacle/Controller/CommentsVC.swift
@@ -9,9 +9,60 @@
 import UIKit
 
 class CommentsVC: UICollectionViewController {
+    
+    override var inputAccessoryView: UIView? {
+        get {
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    var containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .white
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 60)
+        
+        let submitBtn = UIButton(type: .system)
+        submitBtn.setTitle("Submit", for: .normal)
+        submitBtn.setTitleColor(.black, for: .normal)
+        submitBtn.titleLabel?.font = UIFont(name: "Courier-Bold", size: 20)
+        submitBtn.addTarget(self, action: #selector(submitBtnWasPressed), for: .touchUpInside)
+        containerView.addSubview(submitBtn)
+        
+        submitBtn.anchor(top: containerView.topAnchor, left: nil, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 12, width: 90, height: 0)
+        
+        let textField = UITextField()
+        textField.placeholder = "Enter comment"
+        textField.font = UIFont(name: "Courier", size: 20)
+        containerView.addSubview(textField)
+        textField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: submitBtn.leftAnchor, paddingTop: 0, paddingLeft: 12, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        return containerView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "Comments"
+        
         collectionView?.backgroundColor = .purple
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    @objc func submitBtnWasPressed() {
+        print("Handling submit from textfield")
+    }
+    
 }
